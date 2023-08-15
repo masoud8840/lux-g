c00ca23114453ccf92f282897b26693cc2cb1434
Hossein <hosseinpirasbaghi@gmail.com>
2023-08-15 15:31:49 +0330

    updated

--- src/composables/index.js
+++ src/composables/index.js
@@ -1,15 +1,27 @@
-//sing in javascript code
-const $=document;
-const form=$.querySelector(".sing-in")
-const usernameI=$.querySelector('.username-input');
+window.addEventListener("load", () => {
+  let currentPage = window.location.pathname;
+  console.log(currentPage);
+  if (currentPage == "/src/views/sing-In.html") {
+    const form = document.querySelector(".sing-in");
+    const usernameI = document.querySelector(".username-input");
 
-const passwordI=$.querySelector('.password-input');
-form.addEventListener('submit',e=>{
-  e.preventDefault()
-  if(!!usernameI.value && !!passwordI.value) {
-        window.location.href='/src/views/index.html'}
-    else{
-        alert('plz enter your information')
-    }
-console.log(!!usernameI.value && !!passwordI.value);
-})
+    const passwordI = document.querySelector(".password-input");
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      if (!!usernameI.value && !!passwordI.value) {
+        window.location.href = "/src/views/index.html";
+      } else {
+        alert("plz enter your information");
+      }
+      console.log(!!usernameI.value && !!passwordI.value);
+    });
+  } else if (currentPage == "/src/views/shop.html") {
+    const categoryOptions = document.querySelector(".category-options");
+    categoryOptions.addEventListener("click", (e) => {
+      document.querySelector('button.active').classList.remove('active');
+      console.log(e.target);
+      e.target.classList.add('active')
+    });
+    
+  }
+});
